d22ceca25a2046a65e3ad4a9ee0792771811b730
Javier Cavallo <javier.cavallo@globant.com>
2018-09-07 11:12:04 -0300

    Fixed update measure issue

--- digital.caliber.services/Services/MeasureService.cs
+++ digital.caliber.services/Services/MeasureService.cs
@@ -46,6 +46,9 @@ namespace digital.caliber.services.Services
 
         public async Task<int> SaveMeasure(string userId, MeasureViewModel viewModel)
         {
+            if (viewModel.Id > 0)
+                return await UpdateMeasure(viewModel);
+
             var model = new Measures()
             {
                 UserId = userId,
@@ -103,17 +106,7 @@ namespace digital.caliber.services.Services
                 }
             };
 
-            if (viewModel.Id > 0)
-            {
-                model.Id = viewModel.Id;
-                //_context.Measures.Update(model);
-                _context.Entry(model).State = EntityState.Modified;
-            }
-            else
-            {
-                await _context.Measures.AddAsync(model);
-            }
-
+            await _context.Measures.AddAsync(model);
             await _context.SaveChangesAsync();
             return model.Id;
         }
@@ -145,6 +138,66 @@ namespace digital.caliber.services.Services
             result.DateMeasure = data.DateMeasure;
 
             return result;
+        }
+
+        private async Task<int> UpdateMeasure(MeasureViewModel viewModel)
+        {
+            var model = await _context.Measures.Include(x => x.Teeths).Include(x => x.Mouth)
+                .FirstOrDefaultAsync(x => x.Id == viewModel.Id);
+
+            model.PatientName = viewModel.PatientName;
+            model.DateMeasure = DateTime.UtcNow;
+            model.HcNumber = viewModel.HcNumber;
+
+            model.Teeths.Tooth11 = viewModel.Teeths.Tooth11;
+            model.Teeths.Tooth12 = viewModel.Teeths.Tooth12;
+            model.Teeths.Tooth13 = viewModel.Teeths.Tooth13;
+            model.Teeths.Tooth14 = viewModel.Teeths.Tooth14;
+            model.Teeths.Tooth15 = viewModel.Teeths.Tooth15;
+            model.Teeths.Tooth16 = viewModel.Teeths.Tooth16;
+            model.Teeths.Tooth17 = viewModel.Teeths.Tooth17;
+
+            model.Teeths.Tooth21 = viewModel.Teeths.Tooth21;
+            model.Teeths.Tooth22 = viewModel.Teeths.Tooth22;
+            model.Teeths.Tooth23 = viewModel.Teeths.Tooth23;
+            model.Teeths.Tooth24 = viewModel.Teeths.Tooth24;
+            model.Teeths.Tooth25 = viewModel.Teeths.Tooth25;
+            model.Teeths.Tooth26 = viewModel.Teeths.Tooth26;
+            model.Teeths.Tooth27 = viewModel.Teeths.Tooth27;
+
+            model.Teeths.Tooth31 = viewModel.Teeths.Tooth31;
+            model.Teeths.Tooth32 = viewModel.Teeths.Tooth32;
+            model.Teeths.Tooth33 = viewModel.Teeths.Tooth33;
+            model.Teeths.Tooth34 = viewModel.Teeths.Tooth34;
+            model.Teeths.Tooth35 = viewModel.Teeths.Tooth35;
+            model.Teeths.Tooth36 = viewModel.Teeths.Tooth36;
+            model.Teeths.Tooth37 = viewModel.Teeths.Tooth37;
+
+            model.Teeths.Tooth41 = viewModel.Teeths.Tooth41;
+            model.Teeths.Tooth42 = viewModel.Teeths.Tooth42;
+            model.Teeths.Tooth43 = viewModel.Teeths.Tooth43;
+            model.Teeths.Tooth44 = viewModel.Teeths.Tooth44;
+            model.Teeths.Tooth45 = viewModel.Teeths.Tooth45;
+            model.Teeths.Tooth46 = viewModel.Teeths.Tooth46;
+            model.Teeths.Tooth47 = viewModel.Teeths.Tooth47;
+
+            model.Mouth.LeftInferiorCanine = viewModel.Mouth.LeftInferiorCanine;
+            model.Mouth.LeftInferiorIncisive = viewModel.Mouth.LeftInferiorIncisive;
+            model.Mouth.LeftInferiorPremolar = viewModel.Mouth.LeftInferiorPremolar;
+            model.Mouth.LeftSuperiorCanine = viewModel.Mouth.LeftSuperiorCanine;
+            model.Mouth.LeftSuperiorIncisive = viewModel.Mouth.LeftSuperiorIncisive;
+            model.Mouth.LeftSuperiorPremolar = viewModel.Mouth.LeftSuperiorPremolar;
+            model.Mouth.RightInferiorCanine = viewModel.Mouth.RightInferiorCanine;
+            model.Mouth.RightInferiorPremolar = viewModel.Mouth.RightInferiorPremolar;
+            model.Mouth.RightInferiorIncisive = viewModel.Mouth.RightInferiorIncisive;
+            model.Mouth.RightSuperiorCanine = viewModel.Mouth.RightSuperiorCanine;
+            model.Mouth.RightSuperiorIncisive = viewModel.Mouth.RightSuperiorIncisive;
+            model.Mouth.RightSuperiorPremolar = viewModel.Mouth.RightSuperiorPremolar;
+
+            _context.Update(model);
+            await _context.SaveChangesAsync();
+
+            return model.Id;
         }
     }
 }
